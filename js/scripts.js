bdeec1a2d6d6a1eba966c7acce745ad0e91e2305
steven192 <116491188+steven192@users.noreply.github.com>
2023-09-07 04:32:02 +0300

    Add files via upload
    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Landing Page v6.0.6 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under steven (https://github.com/steven192/landingpages_github.io.git/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
